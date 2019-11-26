17d2c42476c2c375bc6eb1fc4e7390c9f2110a91
alex-ovo nathan <avo.nathan@gmail.com>
2019-11-26 10:29:28 +0100

    add admin registration

---
+++ BankServices.Api/Models/CstInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankServices.Api.Models
+{
+    public class CstInfo
+    {
+        public string CstFName { get; set; }
+        public string CstLName { get; set; }
+        public string CstEmail { get; set; }
+        public string CstPhone { get; set; }
+        public string CstAcctType { get; set; }
+
+    }
+}
